5a8d7a2d5fe82ba5960eebd5d20721ea4061bab4
Alex Crits-Christoph <notalexcritschristoph@gmail.com>
2018-03-07 12:38:01 -0800

    updated Comp

--- antibaseComp.py
+++ antibaseComp.py
@@ -7,12 +7,30 @@ import csv
 def importAdductMasses(antiBase_file):
 	with open(antiBase_file) as file:
 		reader = csv.reader(file)
-		adduct_titles = reader.next()
+		adduct_titles = next(reader)
+		print(adduct_titles)
 		antiBase_dict = {}
+		
 		for row in reader:
-			temp_array = [row[x] for x in range(6, len(row))]
-			antiBase_dict[row[0]] = temp_array
-	return antiBase_dict, adduct_title
+			add = True
+			temp_array = []
+			for x in range(6, len(row)):
+				temp_array.append(row[x])
+			for y in temp_array:
+				if not y:
+					add = False  
+			
+
+				#temp_array = [float(row[x]) if row[x] != None for x in range(6, len(row)) ]
+			#print(temp_array)
+			#input("...")
+			#for j in temp_array:
+			#	print(float(j.replace(" ", "")))
+			#print(temp_array)
+			if add:
+				antiBase_dict[row[0]] = [float(z) for z in temp_array]
+				#input("...")
+	return antiBase_dict, adduct_titles
 
 def preprocess_sample(sample, antiBase_dict, adduct_titles, cutoff_percent):
 	print("Reading " + sample)
@@ -39,22 +57,44 @@ def preprocess_sample(sample, antiBase_dict, adduct_titles, cutoff_percent):
 			filtered_spectra_properties_list = []
 			for i in range(0, len(intensity_array_MSI)):
 				if intensity_array_MSI[i]/total_TIC >= cutoff_percent:
-					filtered_spectra_properties_list.append(mzs_MSI[i])
+					filtered_spectra_properties_list.append(float(mzs_MSI[i]))
 					filtered_spectra_properties_list.append(intensity_array_MSI[i])
 					filtered_spectra[num] = filtered_spectra_properties_list
 	scans = None
 	output_mapping_dict = {}
+	print("Scans have been scanned")
+	print("now going through antiBase")
+
 	for spectra_key in filtered_spectra.keys():
 		for antiBase_key in antiBase_dict.keys():
+			#print(filtered_spectra[spectra_key])
+			#print(antiBase_dict[antiBase_key])
+			#input("...")
 			counter = 0
 			antiBase_map = []
 			for j in range(0, len(antiBase_dict[antiBase_key])):
-				if math.abs(antiBase_dict[antiBase_key][j] - filtered_spectra[spectra_key][0])/max(antiBase_dict[antiBase_key][j], filtered_spectra[spectra_key][0])< 0.0001: 
+				base_num = antiBase_dict[antiBase_key][j]
+				spec_num = filtered_spectra[spectra_key][0]
+				#print(base_num, spec_num)
+				#print(type(base_num), type(spec_num))
+				diff = abs(base_num - spec_num)/spec_num
+				if diff < 0.00001:
+					#print(diff)
+					#print("Hit") 
 					counter += 1
 					antiBase_map.append(adduct_titles[j])
-			if counter >=2:
-				output_mapping_dict[spectra_key] = antiBase_map
-				break
+					#input("....")
+				if counter >=2:
+					print("Multiple adducts found")
+					output_mapping_dict[spectra_key] = antiBase_map
+					antiBase_map = None
+					break
+			#if counter >=2:
+			#	break
 
-	return output_mapping_dict
-antiBase_dict = importAdductMasses("new_antiBase_file.csv")
+	#return output_mapping_dict
+import time
+start_time = time.time()
+antiBase_dict,adduct_titles = importAdductMasses("new_antiBase_file.csv")
+preprocess_sample("010818_BH14_pos.mzXML", antiBase_dict,adduct_titles, 0.1)
+print("--- %s seconds ---" % (time.time() - start_time))
